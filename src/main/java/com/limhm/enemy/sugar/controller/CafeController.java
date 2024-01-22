2476bfc1049c467214d0f599f4de068739b7a6fe
pinstinct <gusals3407@gmail.com>
2024-01-22 16:34:42 +0900

    리액티브 프로그래밍 적용

--- src/main/java/com/limhm/enemy/sugar/controller/CafeController.java
+++ src/main/java/com/limhm/enemy/sugar/controller/CafeController.java
@@ -1,9 +1,11 @@
 package com.limhm.enemy.sugar.controller;
 
-import com.limhm.enemy.sugar.component.ExcelExporter;
+import com.limhm.enemy.sugar.common.ExcelExporter;
 import com.limhm.enemy.sugar.domain.CafeDrink;
 import com.limhm.enemy.sugar.factory.CafeMegaCoffeeFactory;
 import com.limhm.enemy.sugar.factory.CafeStarbucksFactory;
+import java.util.Arrays;
+import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
@@ -16,7 +18,7 @@ import reactor.core.publisher.Flux;
 
 import java.io.ByteArrayInputStream;
 import java.util.List;
-import java.util.Map;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/cafe")
@@ -28,24 +30,22 @@ public class CafeController {
     private final CafeMegaCoffeeFactory megaCoffee;
 
     @GetMapping(value = "/menu/down", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity<InputStreamResource> downloadMenu() {
-        Flux<CafeDrink> starbucksMenu = starbucks.createBeverages();
-        Flux<CafeDrink> megaCoffeeMenu = megaCoffee.createBeverages();
+    public Mono<ResponseEntity<InputStreamResource>> downloadMenu() {
+        List<Mono<List<CafeDrink>>> allCafeMenuMono = Arrays.asList(
+            starbucks.createBeverages().collectList(), megaCoffee.createBeverages().collectList());
 
-        List<CafeDrink> a = starbucksMenu.collectList().block();
-        List<CafeDrink> b = megaCoffeeMenu.collectList().block();
+        return Flux.merge(allCafeMenuMono)
+            .collectMap(menu -> menu.get(0).getCafe().getKorName(), Function.identity())
+            .map(allCafeMenu -> {
+                String[] header = {"이름", "칼로리", "포화지방", "당류", "나트륨", "단백질", "카페인"};
+                byte[] excelBytes = excelExporter.generateExcel(allCafeMenu, header);
+                InputStreamResource resource = new InputStreamResource(
+                    new ByteArrayInputStream(excelBytes));
+                HttpHeaders headers = new HttpHeaders();
+                headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=menu.xlsx");
 
-        Map<String, List<CafeDrink>> allCafeMenu = Map.of(a.get(0).getCafe().getKorName(), a,
-            b.get(0).getCafe().getKorName(), b);
-
-        String[] header = {"이름", "칼로리", "포화지방", "당류", "나트륨", "단백질", "카페인"};
-        byte[] excelBytes = excelExporter.generateExcel(allCafeMenu, header);
-        InputStreamResource resource = new InputStreamResource(
-            new ByteArrayInputStream(excelBytes));
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=menu.xlsx");
-
-        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_OCTET_STREAM)
-            .body(resource);
+                return ResponseEntity.ok().headers(headers)
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
+            });
     }
 }
