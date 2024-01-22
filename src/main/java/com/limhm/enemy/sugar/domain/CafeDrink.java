c2c67262a2308cfe73f15eceda79a9312e249ab5
pinstinct <gusals3407@gmail.com>
2024-01-22 16:15:46 +0900

    불필요한 코드 제거

--- src/main/java/com/limhm/enemy/sugar/domain/CafeDrink.java
+++ src/main/java/com/limhm/enemy/sugar/domain/CafeDrink.java
@@ -3,7 +3,6 @@ package com.limhm.enemy.sugar.domain;
 import com.limhm.enemy.sugar.templatemethod.ExcelExportable;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
 
@@ -11,7 +10,6 @@ import java.util.Optional;
 
 @Getter
 @Setter
-@ToString
 public class CafeDrink implements Beverage, ExcelExportable {
 
     private String name;
