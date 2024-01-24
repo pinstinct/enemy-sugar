eb2755db759d1c7e18010d50a4178b30fc0a6482
pinstinct <gusals3407@gmail.com>
2024-01-24 17:34:16 +0900

    주석 추가

--- src/main/java/com/limhm/enemy/sugar/domain/Beverage.java
+++ src/main/java/com/limhm/enemy/sugar/domain/Beverage.java
@@ -2,6 +2,9 @@ package com.limhm.enemy.sugar.domain;
 
 import com.limhm.enemy.sugar.strategy.ExcelExportable;
 
+/**
+ * ExcelExporter 클래스가 ExcelExportable 전략 인터페이스를 구현한 클래스를 파라미터로 받으므로, ExcelExportable 인터페이스를 상속한다.
+ */
 public interface Beverage extends ExcelExportable {
 
     Company getCompany();
