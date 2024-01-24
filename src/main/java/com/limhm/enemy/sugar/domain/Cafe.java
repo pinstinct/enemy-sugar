abaea6282c8ee9acc4a4e1cae64e62ac15598fa0
pinstinct <gusals3407@gmail.com>
2024-01-24 17:21:21 +0900

    Company 인터페이스 추가

--- src/main/java/com/limhm/enemy/sugar/domain/Cafe.java
+++ src/main/java/com/limhm/enemy/sugar/domain/Cafe.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-public class Cafe {
+public class Cafe implements Company {
+
     private String korName;
 }
---
+++ src/main/java/com/limhm/enemy/sugar/domain/Company.java
@@ -0,0 +1,6 @@
+package com.limhm.enemy.sugar.domain;
+
+public interface Company {
+
+    String getKorName();
+}
