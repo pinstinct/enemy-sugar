dcb198c4e00baab076d6cb661c5a82cdf8849872
pinstinct <gusals3407@gmail.com>
2024-02-02 10:51:39 +0900

    CustomException 추가

---
+++ src/main/java/com/limhm/enemy/sugar/exception/ConnectionException.java
@@ -0,0 +1,8 @@
+package com.limhm.enemy.sugar.exception;
+
+public class ConnectionException extends RuntimeException {
+
+    public ConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
